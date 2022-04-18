ecc29400b169c30a87f49f728a322913f7d9629b
吴月雷 <wuyuelei@innovator.cc>
2022-04-18 18:15:00 +0800

    spring-debug

---
+++ spring-debug/src/main/java/com/leiyue/debug/MyAppContext.java
@@ -0,0 +1,9 @@
+package com.leiyue.debug;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyAppContext {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext();
+	}
+}
